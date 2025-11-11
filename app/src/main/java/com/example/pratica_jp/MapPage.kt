067aa280599ad295a38b620f71ff515c4cc83d04
jp <jppb2005normal@gmail.com>
2025-11-11 08:41:10 -0300

    add pin to favorite local

--- app/src/main/java/com/example/pratica_jp/MapPage.kt
+++ app/src/main/java/com/example/pratica_jp/MapPage.kt
@@ -25,6 +25,12 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.example.pratica_jp.model.MainViewModel
 import com.example.pratica_jp.ui.theme.Pratica_jpTheme
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
 
 //class MapPage : ComponentActivity() {
 //    override fun onCreate(savedInstanceState: Bundle?) {
@@ -58,26 +64,46 @@ import com.example.pratica_jp.ui.theme.Pratica_jpTheme
 //    }
 //}
 
+
 @Composable
 fun MapPage(modifier: Modifier = Modifier.Companion, viewModel: MainViewModel) {
     val activity = LocalActivity.current as Activity
+    val recife = LatLng(-8.05, -34.9)
+    val caruaru = LatLng(-8.27, -35.98)
+    val joaopessoa = LatLng(-7.12, -34.84)
 
-    Column(
-        modifier = modifier.fillMaxSize()
-            .background(Color.Gray)
-            .wrapContentSize(Alignment.Center)
-    ) {
-        Text(text = "Mapa",
-            fontWeight = FontWeight.Bold,
-            color = Color.White,
-            modifier = modifier.align(CenterHorizontally),
-            textAlign = TextAlign.Center,
-            fontSize = 20.sp)
+    val camPosState = rememberCameraPositionState ()
 
-        Button(onClick = {
-            activity.finish()
-        }) {
-            Text("Sair")
+
+    GoogleMap (modifier = Modifier.fillMaxSize(), cameraPositionState = camPosState, onMapClick = {
+        viewModel.add("Cidade@${it.latitude}:${it.longitude}", location = it) } )
+    {
+        viewModel.cities.forEach {
+            if (it.location != null) {
+                Marker( state = MarkerState(position = it.location),
+                    title = it.name, snippet = "${it.location}")
+            }
         }
+        Marker(
+            state = MarkerState(position = recife),
+            title = "Recife",
+            snippet = "Marcador em Recife",
+            icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)
+        )
+        Marker(
+            state = MarkerState(position = caruaru),
+            title = "caruaru",
+            snippet = "Marcador em caruaru",
+            icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)
+        )
+        Marker(
+            state = MarkerState(position = joaopessoa),
+            title = "joaopessoa",
+            snippet = "Marcador em joaopessoa",
+            icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)
+        )
+
     }
+
+
 }
