67699809eb74dc09188d174cba904fcf50b68181
jp <jppb2005normal@gmail.com>
2025-11-11 08:27:22 -0300

    add permision to acess map

--- app/src/main/java/com/example/pratica_jp/MainActivity.kt
+++ app/src/main/java/com/example/pratica_jp/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.pratica_jp
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -23,6 +25,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavDestination.Companion.hasRoute
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.pratica_jp.model.MainViewModel
 import com.example.pratica_jp.ui.CityDialog
@@ -30,6 +34,7 @@ import com.example.pratica_jp.ui.HomePage
 import com.example.pratica_jp.ui.nav.BottomNavBar
 import com.example.pratica_jp.ui.nav.BottomNavItem
 import com.example.pratica_jp.ui.nav.MainNavHost
+import com.example.pratica_jp.ui.nav.Route
 import com.example.pratica_jp.ui.theme.Pratica_jpTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -41,6 +46,11 @@ class MainActivity : ComponentActivity() {
             val viewModel : MainViewModel by viewModels()
             val navController = rememberNavController()
             var showDialog by remember { mutableStateOf(false) }
+            val currentRoute = navController.currentBackStackEntryAsState()
+            val showButton = currentRoute.value?.destination?.hasRoute(Route.List::class) == true
+            val launcher = rememberLauncherForActivityResult(contract =
+                ActivityResultContracts.RequestPermission(), onResult = {} )
+
             Pratica_jpTheme {
                 if (showDialog) CityDialog(
                     onDismiss = { showDialog = false },
@@ -79,13 +89,15 @@ class MainActivity : ComponentActivity() {
                     },
 
                     floatingActionButton = {
-
-                        FloatingActionButton(onClick = { showDialog = true }) {
-                            Icon(Icons.Default.Add, contentDescription = "Adicionar")
+                        if (showButton) {
+                            FloatingActionButton(onClick = { showDialog = true }) {
+                                Icon(Icons.Default.Add, contentDescription = "Adicionar")
+                            }
                         }
                     }
                 ) { innerPadding ->
                     Box(modifier = Modifier.padding(innerPadding)) {
+                        launcher.launch(android.Manifest.permission.ACCESS_FINE_LOCATION)
                         MainNavHost(navController = navController, viewModel)
                     }
                 }
